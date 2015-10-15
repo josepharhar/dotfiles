6a379111795dab978b2d9b2c5579daf939d967c4
Cameron Bratcher <cameronbratcher@gmail.com>
2015-10-15 15:27:46 -0700

    Added colorized output, works on linux, not sure about cygwin

--- install.sh
+++ install.sh
@@ -1,55 +1,59 @@
 #/bin/sh
 
-if [ `uname -o` = "Cygwin" ]; then
-    echo "setting up for Cygwin"
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
 
-    echo "installing apt-cyg"
+if [ `uname -o` = "Cygwin" ]; then
+    echo -e "${BLUE}Setting up for ${RED}Cygwin${NC}"
+
+    echo -e "${BLUE}Installing apt-cyg${NC}"
     lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg && install apt-cyg /bin
 
-    echo "installing zsh"
+    echo -e "${BLUE}Installing zsh${NC}"
     apt-cyg install zsh && mkpasswd -c | sed -e 'sX/bashX/zshX' | tee -a /etc/passwd
 
-    echo "installing vim"
+    echo -e "${BLUE}Installing vim${NC}"
     apt-cyg install vim
 
-    echo "installing screen"
+    echo -e "${BLUE}Installing screen${NC}"
     apt-cyg install screen
 
-    echo "installing git"
+    echo -e "${BLUE}Installing git${NC}"
     apt-cyg install git
 
-elif [[ `uname -o` = "GNU/Linux" ]]; then
+elif [ `uname -o` = "GNU/Linux" ]; then
     #check specific distro
     if [ -f /etc/arch-release ] ; then
         #distro is based on arch
-        echo "Setting up for Arch"
+        echo -e "${BLUE}Setting up for ${RED}Arch${NC}"
 
-        echo "Installing zsh"
+        echo -e "${BLUE}Installing zsh${NC}"
         sudo pacman -S zsh && mkpasswd -c | sed -e 'sX/bashX/zshX' | tee -a /etc/passwd
 
-        echo "Installing vim"
+        echo -e "${BLUE}Installing vim${NC}"
         sudo pacman -S vim
 
-        echo "Installing screen"
+        echo -e "${BLUE}Installing screen${NC}"
         sudo pacman -S screen
 
-        echo "Installing git"
+        echo -e "${BLUE}Installing git${NC}"
         sudo pacman -S git
 
     elif [ -f /etc/debian-release ] ; then
         #distro based on debian (ubuntu)
-        echo "Setting up for debian based (ubuntu)"
+        echo -e "${BLUE}Setting up for ${RED}debian${NC}"
 
-        echo "Installing zsh"
+        echo -e "${BLUE}Installing zsh${NC}"
         sudo apt-get install zsh && mkpasswd -c | sed -e 'sX/bashX/zshX' | tee -a /etc/passwd
         
-        echo "Installing vim"
+        echo -e "${BLUE}Installing vim${NC}"
         sudo apt-get install vim
 
-        echo "Installing screen"
+        echo -e "${BLUE}Installing screen${NC}"
         sudo apt-get install screen
 
-        echo "Installin git"
+        echo -e "${BLUE}Installing git${NC}"
         sudo apt-get install git
 
     fi
