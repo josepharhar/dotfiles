53221c7c8bf49c0e9de472c5b1530b663bf9ad62
Joseph Arhar <josepharhar@gmail.com>
2016-10-13 10:51:55 -0700

    added link script, .vim, pathogen.vim, vim-unimpaired

---
+++ link.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+link() {
+  if [ -h $2 ]; then
+    echo "symlink $2 already exists, skipping"
+    return
+  fi
+
+  if [ -e $2 ]; then
+    echo "file $2 already exists, moving to ${2}.backup"
+    mv $2 ${2}.backup
+  fi
+
+  echo "ln -s $1 $2"
+  ln -s $1 $2
+}
+
+link ~/dotfiles/vim ~/.vim
+link ~/dotfiles/vimrc ~/.vimrc
+link ~/dotfiles/screenrc ~/.screenrc
+link ~/dotfiles/zshrc ~/.zshrc
+link ~/dotfiles/gitconfig ~/.gitconfig
