3ded82d5187783fb68246945d93d7fdaedd8c5f7
Joseph Arhar <josepharhar@gmail.com>
2019-01-19 12:52:18 -0800

    clone

--- anyshrc.sh
+++ anyshrc.sh
@@ -67,8 +67,14 @@ alias gfmt="git diff -U0 --no-color HEAD^ | clang-format-diff.py -i -p1"
 alias gg="git grep -i"
 remote() {
   git remote remove ${2:-origin} || true
-  git remote add ${2:-origin} git://github.com/josepharhar/$1
+  git remote add ${2:-origin} git://github.com/josepharhar/$1 && \
   git remote set-url ${2:-origin} --push git@github.com:josepharhar/$1
+}
+clone() {
+  git clone git://github.com/josepharhar/${1} && \
+  cd $1 && \
+  remote $1 && \
+  git fetch
 }
 
 # open all files from a list, works with aliases - "vimo gdfnh"
