324ec031dc243b8b56968e7edd3a7da53de0c261
Joseph Arhar <josepharhar@gmail.com>
2019-03-01 11:19:41 -0800

    alias npc

--- anyshrc.sh
+++ anyshrc.sh
@@ -277,6 +277,7 @@ alias bbs="ancrcr"
 alias lbbt="GOMA_DISABLED=true ltest"
 alias lbbs="GOMA_DISABLED=true ancrcr"
 alias dtfix="(csd && npx eslint front_end --fix && npm run closure && git cl format --js)"
+alias npc="npr closure"
 # old chrome aliases
 ##alias gng="gn gen out/Default --args='is_chromecast=true is_debug=true"
 #alias gng="gn gen out_chromecast_desktop/debug --args='is_chromecast=true is_debug=true use_goma=true chromecast_branding=\"internal\"'"
