a8c51640af482dc926fd24b20876fa509f57b4b0
Joseph Arhar <josepharhar@gmail.com>
2021-02-24 18:11:34 -0800

    fix bash

--- anyshrc.sh
+++ anyshrc.sh
@@ -304,7 +304,7 @@ alias nothrottle="sudo dnctl pipe 1 config bw 0"
 if ! [ -z "$JARHAR_OSX" ]; then
   # r508527 is the oldest one that i can still open devtools on with my mac
   alias bisect="python tools/bisect-builds.py -g 508527 -a mac64 --use-local-cache --"
-else
+#else
   #alias bisect="python tools/bisect-builds.py -a mac64 --use-local-cache --"
 fi
 
